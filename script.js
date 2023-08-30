d1f87b8366697a56f5e859242963df7d4621cbb6
Abhinav Japesh <47645327+akjapesh@users.noreply.github.com>
2023-08-30 23:09:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let foodItems=[{name:"five-js",rating:"3"},{name:"six-js",rating:"3"},{name:"seven-js",rating:"5"},{name:"eight-js",rating:"4"},{name:"nine-js",rating:"2"}];
+
 function createItem(food){
     let htmlContent=`<div class="dish-card">
     <div class="image-pos"></div>
@@ -19,17 +20,61 @@ let newFoodItem=document.createElement('div');
 // newFoodItem.classList.add("dish-card");
 newFoodItem.innerHTML=htmlContent;
 return newFoodItem;
-}
+};
+
 function fillItems(){
 let parentDiv=document.getElementsByClassName("show-rest")[0];
-console.log(parentDiv[0]);
 foodItems.forEach(food=>{
     let newFodItem=createItem(food);
     parentDiv.appendChild(newFodItem);
 })
+};
+
+function leftClickAction(shift){
+    console.log("left clicked")
+    let dishCards=document.getElementsByClassName("inside-box");
+    Array.prototype.map.call(dishCards,(card)=>(card.style.translate=`-${shift}%`));
+}
+function rightClickAction(shift){
+    console.log("right clicked");
+    let dishCards=document.getElementsByClassName("inside-box");
+    // dishCards.map((card)=>(card.style.translate-=100));
+    Array.prototype.map.call(dishCards,(card)=>(card.style.translate=`-${shift}%`));
 }
 
-// document.onload(()=>{
-//     fillItems();
-// })
+let maxShift=document.getElementsByClassName("inside-box").length-2;
+let presentShift=0;
+
+function isHidden({type}){
+    if(type=="left")
+        return presentShift==0;
+    return presentShift==maxShift;
+}
+
+function clickAction({type}){
+    if(type=="left"&&presentShift){
+        presentShift--;
+        rightBut.style.visibility="visible";
+        leftClickAction(presentShift*100);
+        if(isHidden({type})){
+            leftBut.style.visibility="hidden";
+        }
+        
+    }
+    else if(type=="right"&&(presentShift<=maxShift)){
+        presentShift++;
+        leftBut.style.visibility="visible";
+        rightClickAction(presentShift*100);
+        if(isHidden({type})){
+            rightBut.style.visibility="hidden";
+        }
+        
+    } 
+}
+let leftBut=document.getElementsByClassName("left-slide-but")[0];
+let rightBut=document.getElementsByClassName("right-slide-but")[0];
+leftBut?.addEventListener('click',()=>clickAction({type:"left"}));
+rightBut?.addEventListener('click',()=>clickAction({type:"right"}));
+
 fillItems();
+
